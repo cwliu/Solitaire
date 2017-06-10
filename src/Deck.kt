de3fe755a1e18b454f4cf515700a7a991bd12b21
Cody Liu <liu@codylab.com>
2017-06-10 16:33:22 +1000

    add two helper functions

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,6 +1,16 @@
+import java.util.*
+
 class Deck {
 
     val cards = Array(52, { Card(it % 13, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0)
+
+    fun reset() = {
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(i: Int): String = when (i / 13) {
         0 -> "Clubs"
