e4327f71cb4f4c92181f486473edf02050768d65
Cody Liu <liu@codylab.com>
2017-06-10 14:37:27 +1000

    create a Card class in kotlin

--- src/Card.kt
+++ src/Card.kt
@@ -1,1 +1,9 @@
-class Card(val value: Int, val suit: String, val isFaceUp: Boolean)
+/**
+ * Created by cwliu on 10/06/2017.
+ */
+
+class Card {
+    val value: Int = 0
+    val suit: String = ""
+    var faceUp: Boolean = false
+}
