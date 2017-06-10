95ac670179744a9f2090b572ea1a5a8122767992
Cody Liu <liu@codylab.com>
2017-06-10 15:21:00 +1000

    simplify Kotlin class

--- src/Card.kt
+++ src/Card.kt
@@ -2,8 +2,4 @@
  * Created by cwliu on 10/06/2017.
  */
 
-class Card {
-    val value: Int = 0
-    val suit: String = ""
-    var faceUp: Boolean = false
-}
+class Card (val value: Int, val suit: String, var faceUp: Boolean)
