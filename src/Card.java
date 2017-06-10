25763149be3b1e427f9e0552a3f214651e2e6270
Cody Liu <liu@codylab.com>
2017-06-10 14:06:22 +1000

    Add Card of Java

---
+++ src/Card.java
@@ -0,0 +1,23 @@
+public class Card {
+    private int value;
+    private String suit;
+    private boolean faceUp;
+
+    public Card(int value, String suit, boolean faceUp) {
+        this.value = value;
+        this.suit = suit;
+        this.faceUp = faceUp;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public boolean isFaceUp() {
+        return faceUp;
+    }
+}
