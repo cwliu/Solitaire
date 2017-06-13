2baa5f95ccb8e10ac7e3d5b9951ac8675ca039a8
Cody Liu <liu@codylab.com>
2017-06-13 15:30:11 +1000

    Refine a testcase

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ CLI Solitaire game using Kotlin
 
 ## Screenshot
 
-![](https://i.imgur.com/XgGvBQE.png)
+![](https://i.imgur.com/0ApNcgM.png)
 
 ![](https://i.imgur.com/2gQeiUS.png)
--- test/GameTest.kt
+++ test/GameTest.kt
@@ -1,31 +1,34 @@
 import org.junit.Test
+
 class GameTest {
 
     @Test
-    fun kingInFirstFoundationPile(){
+    fun kingInFirstFoundationPile() {
         // arrange
         var numGames = 0
         val maxGames = 10000
 
         // act
-        for (i in 1..maxGames){
+        for (i in 1..maxGames) {
             numGames++
             GameModel.resetGame()
-            for (j in 1..100){
+            for (j in 1..100) {
                 GamePresenter.onDeckTap()
                 GamePresenter.onWasteTap()
                 GameModel.tableauPiles.forEachIndexed { index, tableauPile ->
-                 GamePresenter.onTableauTab(index, tableauPile.cards.lastIndex)
+                    GamePresenter.onTableauTab(index, tableauPile.cards.lastIndex)
+                }
+
+                if (GameModel.foundationPiles[0].cards.size == 13) {
+                    // assert
+                    GameModel.debugPrint()
+                    println("# Game: $numGames")
+                    assert(numGames < maxGames)
+                    break
                 }
             }
 
-            if(GameModel.foundationPiles[0].cards.size == 13){
-                // assert
-                GameModel.debugPrint()
-                println("# Game: $numGames" )
-                assert(numGames < maxGames)
-                break
-            }
+
 
         }
     }
